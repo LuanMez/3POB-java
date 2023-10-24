db9ef341185d26295d1eee397e9e7233de8b9b6e
Luan Moreira de Menezes <125929128+LuanMez@users.noreply.github.com>
2023-10-23 23:33:59 -0300

    Main.java

--- AV1/Main.java
+++ AV1/Main.java
@@ -1,10 +1,8 @@
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Menu menu = new Menu();
-		menu.Processamento();
-
-	}
+    public static void main(String[] args) {
+        Menu menu = new Menu();
+        menu.processamento();
+    }
 
 }
